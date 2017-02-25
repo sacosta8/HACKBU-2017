ecfe1e75a2426b40c4600498bf91944a9550a0bf
sacosta8 <sacosta8@binghamton.edu>
2017-02-25 14:35:24 -0500

    pushed p5 stuff

---
+++ static/javascript/sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+  createCanvas(100,100);
+  frameRate(60);
+  background(250);
+  imag = loadImage("static/imgs/sonic.png",function(img));
+  image(img,100,100);
+}
+
+function draw() {
+  
+}
