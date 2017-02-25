bbb86f2ad4324a5be75af8187acae7aa4451d12f
Aniesk <aorella1@binghamton.edu>
2017-02-25 14:55:52 -0500

    API key

--- static/javascript/kairos.js
+++ static/javascript/kairos.js
@@ -1,5 +1,5 @@
 /* * * Kairos Javascript SDK * * * *
-* Authored by Eric Turner 
+* Authored by Eric Turner
 * http://kairos.com
 */
 
@@ -9,7 +9,7 @@
 /* Constructor - Creates and returns an instance of the Kairos client
   @param app_id  : your app_id
   @param app_key : your app_key */
-var Kairos = function(app_id, app_key) 
+var Kairos = function(cd7690fd, 32df39bf4f4e7d9882a9e6e9154ceaca)
 {
   this.app_id   = app_id;
   this.app_key  = app_key;
@@ -21,10 +21,10 @@ var Kairos = function(app_id, app_key)
 
 /* Kairos requires jQuery */
 function isJQueryAvailable() {
-    
+
   if (typeof jQuery === 'undefined') {
       return false;
-  } 
+  }
 
   return true;
 }
@@ -34,7 +34,7 @@ function isJQueryAvailable() {
 
 /* Authentication checker */
 Kairos.prototype.authenticationProvided = function() {
-    
+
   if((!this.app_key) || (!this.app_id))
   {
       return false;
@@ -47,7 +47,7 @@ Kairos.prototype.authenticationProvided = function() {
 
 /* Detect faces in an image
   @param image_data : this is the base64 data of the image
-  @param callback   : your callback function will be called when the request completes 
+  @param callback   : your callback function will be called when the request completes
   @param options    : [Optional] an object containing any additional parameters you wish to append to the request */
 Kairos.prototype.detect = function(image_data, callback, options) {
 
@@ -70,7 +70,7 @@ Kairos.prototype.detect = function(image_data, callback, options) {
     console.log('Kairos Error: the callback parameter is required and must be of type [function]');
     return;
   }
-  
+
   var url = this.api_host + 'detect';
 
 	var data = { 'image' : image_data };
@@ -102,7 +102,7 @@ Kairos.prototype.detect = function(image_data, callback, options) {
   @param image_data : this is the base64 data of the image
   @param gallery_id : the gallery name you want to enroll into
   @param subject_id : the subject id you want to enroll the image under
-  @param callback   : your callback function will be called when the request completes 
+  @param callback   : your callback function will be called when the request completes
   @param options    : [Optional] an object containing any additional parameters you wish to append to the request */
 Kairos.prototype.enroll = function(image_data, gallery_id, subject_id, callback, options) {
 
@@ -135,7 +135,7 @@ Kairos.prototype.enroll = function(image_data, gallery_id, subject_id, callback,
     console.log('Kairos Error: the callback parameter is required and must be of type [function]');
     return;
   }
-  
+
   var url = this.api_host + 'enroll';
 
   var data = { 'image' : image_data , 'gallery_name' : gallery_id, 'subject_id' : subject_id};
@@ -166,7 +166,7 @@ Kairos.prototype.enroll = function(image_data, gallery_id, subject_id, callback,
 /* Recognize faces in an image
   @param image_data : this is the base64 data of the image
   @param gallery_id : the gallery name you want to enroll into
-  @param callback   : your callback function will be called when the request completes 
+  @param callback   : your callback function will be called when the request completes
   @param options    : [Optional] an object containing any additional parameters you wish to append to the request */
 Kairos.prototype.recognize = function(image_data, gallery_id, callback, options) {
 
@@ -189,7 +189,7 @@ Kairos.prototype.recognize = function(image_data, gallery_id, callback, options)
     console.log('Kairos Error: the callback parameter is required and must be of type [function]');
     return;
   }
-  
+
   var url = this.api_host + 'recognize';
 
   var data = { 'image' : image_data , 'gallery_name' : gallery_id };
@@ -219,7 +219,7 @@ Kairos.prototype.recognize = function(image_data, gallery_id, callback, options)
 
 
 /* Lists the names of all of your galleries
-  @param callback   : your callback function will be called when the request completes 
+  @param callback   : your callback function will be called when the request completes
   @param options    : [Optional] an object containing any additional parameters you wish to append to the request */
 Kairos.prototype.viewGalleries = function(callback, options) {
 
@@ -237,7 +237,7 @@ Kairos.prototype.viewGalleries = function(callback, options) {
     console.log('Kairos Error: the callback parameter is required and must be of type [function]');
     return;
   }
-  
+
   var url = this.api_host + 'gallery/list_all';
 
   var data = { };
@@ -268,7 +268,7 @@ Kairos.prototype.viewGalleries = function(callback, options) {
 
 /* Lists all subjects currently enrolled in a given gallery
   @param gallery_id : the gallery name for the gallery you wish to return info about
-  @param callback   : your callback function will be called when the request completes 
+  @param callback   : your callback function will be called when the request completes
   @param options    : [Optional] an object containing any additional parameters you wish to append to the request */
 Kairos.prototype.viewSubjectsInGallery = function(gallery_id, callback, options) {
 
@@ -291,7 +291,7 @@ Kairos.prototype.viewSubjectsInGallery = function(gallery_id, callback, options)
     console.log('Kairos Error: the callback parameter is required and must be of type [function]');
     return;
   }
-  
+
   var url = this.api_host + 'gallery/view';
 
   var data = { 'gallery_name' : gallery_id };
@@ -341,7 +341,7 @@ Kairos.prototype.removeGallery = function(gallery_id, callback, options) {
     console.log('Kairos Error: the gallery_id parameter is required');
     return;
   }
-  
+
   var url = this.api_host + 'gallery/remove';
 
   var data = { 'gallery_name' : gallery_id };
@@ -394,7 +394,7 @@ Kairos.prototype.removeSubjectFromGallery = function(subject_id, gallery_id, cal
     console.log('Kairos Error: the subject_id parameter is required');
     return;
   }
-  
+
   var url = this.api_host + 'gallery/remove_subject';
 
   var data = { 'gallery_name' : gallery_id, 'subject_id' : subject_id};
@@ -423,7 +423,7 @@ Kairos.prototype.removeSubjectFromGallery = function(subject_id, gallery_id, cal
   @param image : this is the base64 data of the image or a publicly accessible URL
   @param gallery_id : the gallery name you want to check
   @param subject_id : an enrolled subject you want to check against
-  @param callback   : your callback function will be called when the request completes 
+  @param callback   : your callback function will be called when the request completes
   @param options    : [Optional] an object containing any additional parameters you wish to append to the request */
 Kairos.prototype.verify = function(image, gallery_id, subject_id, callback, options) {
 
@@ -456,7 +456,7 @@ Kairos.prototype.verify = function(image, gallery_id, subject_id, callback, opti
     console.log('Kairos Error: the callback parameter is required and must be of type [function]');
     return;
   }
-  
+
   var url = this.api_host + 'verify';
 
   var data = { 'image' : image , 'gallery_name' : gallery_id , 'subject_id' : subject_id };
@@ -497,7 +497,7 @@ Kairos.prototype.checkAuthentication = function(callback, options) {
     console.log('Kairos Error: the callback parameter is required and must be of type [function]');
     return;
   }
-  
+
   var url = this.api_host + 'gallery/list_all';
 
   var data = { };
