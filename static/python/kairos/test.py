bfe3fb40fde12b0f83ddf72daf2c234366d3456e
Aniesk <aorella1@binghamton.edu>
2017-02-26 12:51:00 -0500

    FINAL COMMIT OF HACKBU!

--- static/python/kairos/test.py
+++ static/python/kairos/test.py
@@ -5,20 +5,6 @@ kairos_face.settings.app_id = "cd7690fd"
 kairos_face.settings.app_key = "32df39bf4f4e7d9882a9e6e9154ceaca"
 
 
-name_list = kairos_face.get_gallery('a-gallery')["subject_ids"]
-print(name_list)
-top_five = [0]
-
-for name in name_list:
-    recognized_faces = kairos_face.verify_face(url="https://upload.wikimedia.org/wikipedia/commons/thumb/2/21/George_Hotz.jpg/200px-George_Hotz.jpg",subject_id = name ,gallery_name= "a-gallery")
-    percent = recognized_faces["images"][0]["transaction"]["confidence"]*100
-    top_five.append(percent)
-    top_five.sort(reverse=True);
-    if len(top_five) > 5:
-        del(top_five[5])
-
-print(top_five)
-
 
 #image, subject_id, gallery_name,
 
